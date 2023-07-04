70b848437870e0622079e93691de6bfdd0234d77
Lushih Hsieh <bruce@mail.kh.edu.tw>
2023-07-04 10:43:57 +0800

    Add more scripts.

---
+++ FetchSongLyrics.py
@@ -0,0 +1,12 @@
+i# pip install lyricsgenius
+
+import lyricsgenius
+
+api_key = "xxxxxxxxxxxxxxxxxxxxx"
+
+genius = lyricsgenius.Genius(api_key)
+artist = genius.search_artist("Pop Smoke", max_songs=5,sort="title")
+song = artist.song("100k On a Coupe")
+
+print(song.lyrics)
+
